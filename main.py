00ece897f159bb7484ee75e64ed5f325ae87c08b
RafaelaAfférri <rafaelaao@al.insper.edu.br>
2024-04-08 11:25:22 -0300

    mudança da distancia focal

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ altura = 480
 distancia_focal = 200
 rodando = True
 angulo = 0
-aumento = 0.1
+aumento = 0.02
 posicao_jogador = np.array([0, 0, 0, 1])
 
 pg.init()
@@ -43,29 +43,6 @@ vertices = vertices.T * 50
 vertices = np.vstack((vertices, np.ones((1, vertices.shape[1]))))
 posicao = np.array([0, 0, 0, 1])
 
-cores = [
-    (255, 0, 0),      # Red
-    (255, 165, 0),    # Orange
-    (255, 255, 0),    # Yellow
-    (0, 255, 0),      # Lime
-    (0, 255, 255),    # Cyan
-    (0, 0, 255),      # Blue
-    (128, 0, 128),    # Purple
-    (255, 0, 255),    # Magenta
-    (128, 128, 128),  # Gray
-    (255, 255, 255),  # White
-    (0, 0, 0),        # Black
-    (255, 192, 203),  # Pink
-    (0, 128, 0),      # Green
-    (128, 0, 0),      # Maroon
-    (0, 128, 128),    # Teal
-    (165, 42, 42),    # Brown
-    (255, 255, 224),  # Light Yellow
-    (255, 140, 0),    # Dark Orange
-    (0, 139, 139),    # Dark Cyan
-    (139, 0, 139)     # Dark Magenta
-]
-
 rodando = True
 
 while rodando:
@@ -76,26 +53,30 @@ while rodando:
         if event.type == pg.KEYDOWN:
             # Altera a velocidade do personagem
             if event.key == pg.K_UP:
-                aumento += 0.1
-            if event.key == pg.K_DOWN:
-                aumento -= 0.1
+                aumento += 0.01
+            if event.key == pg.K_DOWN and aumento > 0.02:
+                aumento -= 0.01
+
+        if event.type == pg.MOUSEWHEEL:
+            if event.y > 0 and distancia_focal<1460:
+                distancia_focal += 25
+            elif event.y < 0 and distancia_focal>26:
+                distancia_focal -= 25
 
     angulo += aumento
-    # angulo += 0.01
     tela.fill((0,0,0))
 
     vertices_rotacionados = translacao(0,0,0) @ translacao(0, 0, 250) @ rotacao_x(angulo) @ rotacao_y(angulo) @ rotacao_z(angulo) @ vertices
     pontos_projetados = projecao_cubo(vertices_rotacionados, distancia_focal)
 
-    cor = random.choice(cores)
 
     for vertice in arestas :
 
         ponto_inicial = (pontos_projetados[vertice[0]][0] + (largura/2), pontos_projetados[vertice[0]][1] + altura/2)
         ponto_final = (pontos_projetados[vertice[1]][0] + (largura/2), pontos_projetados[vertice[1]][1] + altura/2)
-        pg.draw.line(tela, cor, ponto_inicial, ponto_final, 4)
+        pg.draw.line(tela, (255, 0, 150), ponto_inicial, ponto_final, 4)
 
-    pg.display.flip()
+    pg.display.update()
     clock.tick(50)
 
 pg.quit()
