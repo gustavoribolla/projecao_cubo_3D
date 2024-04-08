4ec22f9433ad38647c4b235da6f43d3166cf00c0
RafaelaAfférri <rafaelaao@al.insper.edu.br>
2024-04-07 22:13:32 -0300

    cubo rotaciona?

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ import numpy as np
 import pygame as pg
 from functions import *
 
-largura = 480
-altura = 640
+largura = 640
+altura = 480
 distancia_focal = 200
 rodando = True
-angulo_rotacao = 0
+angulo = 0
+posicao_jogador = np.array([0, 0, 0, 1])
+
+
+pg.init()
 tela = pg.display.set_mode((largura, altura))
 clock = pg.time.Clock()
+
+vertices = np.array([
+    [-1, -1, -1],
+    [-1, -1,  1],
+    [-1,  1, -1],
+    [-1,  1,  1],
+    [ 1, -1, -1],
+    [ 1, -1,  1],
+    [ 1,  1, -1],
+    [ 1,  1,  1]])
 
 arestas = [
     (0, 1),
@@ -24,39 +38,32 @@ arestas = [
     (5, 7),
     (6, 7)]
 
-vertices = np.array([
-    [-1, -1, -1],
-    [-1, -1, 1],
-    [-1, 1, -1],
-    [-1, 1, 1],
-    [1, -1, -1],
-    [1, -1, 1],
-    [1, 1, -1],
-    [1, 1, 1],])
-
-vertices = vertices.T*50
+vertices = vertices.T * 50
 vertices = np.vstack((vertices, np.ones((1, vertices.shape[1]))))
+posicao = np.array([0, 0, 0, 1])
+
+
+rodando = True
 
 while rodando:
-    for evento in pg.event.get():
-        if evento.type == pg.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
             rodando = False
 
-    projecao_pontos = projecao_cubo(vertices, distancia_focal)
-    tela.fill((0, 0, 0))
+    angulo += 0.01
+    tela.fill((0,0,0))
 
-    for aresta in arestas:
-        inicio = (projecao_pontos[aresta[0]][0] + largura/2, projecao_pontos[aresta[0]][1] + altura/2)
-        fim = (projecao_pontos[aresta[1]][0] + largura/2, projecao_pontos[aresta[1]][1] + altura/2)
+    vertices_rotacionados = translacao(0,0,0) @ translacao(0, 0, 250) @ rotacao_x(angulo) @ rotacao_y(angulo) @ rotacao_z(angulo) @ vertices
+    
+    pontos_projetados = projecao_cubo(vertices_rotacionados, distancia_focal)
 
-        distancia_inicio = projecao_pontos[aresta[0]][2]
-        distancia_fim = projecao_pontos[aresta[1]][2]
-        linha_largura = 1/(distancia_inicio/1400 + distancia_fim/1400)
+    for vertice in arestas :
+        ponto_inicial = (pontos_projetados[vertice[0]][0] + (largura/2), pontos_projetados[vertice[0]][1] + altura/2)
+        ponto_final = (pontos_projetados[vertice[1]][0] + (largura/2), pontos_projetados[vertice[1]][1] + altura/2)
+        pg.draw.line(tela, (255, 255, 255), ponto_inicial, ponto_final, 4)
 
-        if projecao_pontos[aresta[0]][2] > 0 and projecao_pontos[aresta[1]][2] > 0 and linha_largura < 30:
-            pg.draw.line(tela, (255, 255, 255), inicio, fim, int(linha_largura))
-        
     pg.display.flip()
-    clock.tick(60)
+    clock.tick(50)
 
 pg.quit()
