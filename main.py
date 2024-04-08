7818e789a99a3462813696ff69e926c52ca2d93f
RafaelaAfférri <rafaelaao@al.insper.edu.br>
2024-04-08 10:28:59 -0300

    more colors

--- main.py
+++ main.py
@@ -44,14 +44,28 @@ vertices = np.vstack((vertices, np.ones((1, vertices.shape[1]))))
 posicao = np.array([0, 0, 0, 1])
 
 cores = [
-    (255, 0, 0),    # Red
-    (255, 165, 0),  # Orange
-    (255, 255, 0),  # Yellow
-    (0, 255, 0),    # Green
-    (0, 0, 255),    # Blue
-    (75, 0, 130),   # Indigo
-    (128, 0, 128)   # Violet
+    (255, 0, 0),      # Red
+    (255, 165, 0),    # Orange
+    (255, 255, 0),    # Yellow
+    (0, 255, 0),      # Lime
+    (0, 255, 255),    # Cyan
+    (0, 0, 255),      # Blue
+    (128, 0, 128),    # Purple
+    (255, 0, 255),    # Magenta
+    (128, 128, 128),  # Gray
+    (255, 255, 255),  # White
+    (0, 0, 0),        # Black
+    (255, 192, 203),  # Pink
+    (0, 128, 0),      # Green
+    (128, 0, 0),      # Maroon
+    (0, 128, 128),    # Teal
+    (165, 42, 42),    # Brown
+    (255, 255, 224),  # Light Yellow
+    (255, 140, 0),    # Dark Orange
+    (0, 139, 139),    # Dark Cyan
+    (139, 0, 139)     # Dark Magenta
 ]
+
 
 
 rodando = True
